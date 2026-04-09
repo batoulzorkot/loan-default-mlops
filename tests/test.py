847271d5d82bd3d693862b6d9912f6e20c92aee7
Batoul Zorkot <bbatoulz@outlook.com>
2026-04-09 02:14:45 +0200

    fix: test.py path correction

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,8 @@
-from app.app import *
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app.app import load_data
 
 def test_load_data():
     df = load_data()
